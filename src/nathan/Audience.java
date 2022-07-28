967edb15607212a0fd977fc5eb1bdf16e7352ead
nathan29849 <phs960418@gmail.com>
2022-07-28 17:00:23 +0900

    refactor: [#1] TicketSeller와 Autience 사이 결합도 낮추기:

--- src/nathan/Audience.java
+++ src/nathan/Audience.java
@@ -11,4 +11,13 @@ public class Audience {
 	public Bag getBag() {
 		return bag;
 	}
+
+	public Long buy(Ticket ticket) {
+		bag.setTicket(ticket);
+		if (!bag.hasInvitation()) {
+			bag.minusAmount(ticket.getFee());
+			return ticket.getFee();
+		}
+		return 0L;
+	}
 }
--- src/nathan/TicketSeller.java
+++ src/nathan/TicketSeller.java
@@ -8,19 +8,9 @@ public class TicketSeller {
 		this.ticketOffice = ticketOffice;
 	}
 
-	public TicketOffice getTicketOffice() {
-		return ticketOffice;
-	}
-
 	public void sellTo(Audience audience) {
-		if (audience.getBag().hasInvitation()) {
-			Ticket ticket = ticketOffice.getTicket();
-			audience.getBag().setTicket(ticket);
-		} else {
-			Ticket ticket = ticketOffice.getTicket();
-			audience.getBag().minusAmount(ticket.getFee());
-			ticketOffice.plusAmount(ticket.getFee());
-			audience.getBag().setTicket(ticket);
-		}
+		Ticket ticket = ticketOffice.getTicket();
+		Long fee = audience.buy(ticket);
+		ticketOffice.plusAmount(fee);
 	}
 }
